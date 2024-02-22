4f2e7fff8386b933dc8754717bbab6426067e6f0
MattiaPaiano91 <paianomattia1@gmail.com>
2024-02-22 15:07:09 +0100

    -add nav bar to all the pages

--- resources/views/aboutUs.blade.php
+++ resources/views/aboutUs.blade.php
@@ -1,11 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>About us</title>
 </head>
+
 <body>
-    <h1>About us</h1>
+    <header>
+        <nav class="d-flex justify-content-between ">
+            <h1 class="p-5">
+                Il mio primo progetto in laravel!
+            </h1>
+            <ul class="d-flex p-5 ">
+                <li class="me-3 list-unstyled">
+                    <a class="text-decoration-none  text-dark" href="{{ url('/contacts') }}">Contacts</a>
+                </li>
+                <li class="me-3 list-unstyled">
+                    <a class="text-decoration-none text-dark" href="{{ url('/aboutUs') }}">About us</a>
+                </li>
+                <li class="me-3 list-unstyled">
+                    <a class="text-decoration-none text-dark" href="{{ url('/shop') }}">Shop</a>
+                </li>
+                <li class="me-3 list-unstyled">
+                    <a class="text-decoration-none text-dark" href="{{ url('/') }}">Home</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
 </body>
+
 </html>
